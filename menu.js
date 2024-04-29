952ca3ef348b07d02518f174ac32843f54e73551
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-04-29 13:38:31 -0300

    acrecentei mais etapas em meu menu js .

--- menu.js
+++ menu.js
@@ -1,5 +1,13 @@
 const btnMenu=document.getElementById("hamburguer-button")
 
 btnMenu.classList.add("hamburguer-button-js-enabled")
+btnMenu.setAttribute("aria-expanded","false")
 
-btnMenu.addEventListener("click")
+btnMenu.addEventListener("click",function(){
+
+    let expanded = this.getAttribute("aria-expanded") === "true"? true: false
+
+    if(expanded){
+
+    }
+})
